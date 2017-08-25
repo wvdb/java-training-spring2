df7de1929e44ec62f3712e8b012404c7fc1ba1f7
wvdbrande <wim.vandenbrande@one-agency.be>
2017-08-25 14:50:20 +0200

    first commit

---
+++ src/main/java/be/ictdynamic/training/Spring2/Spring2Application.java
@@ -0,0 +1,12 @@
+package be.ictdynamic.training.Spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2Application.class, args);
+	}
+}
---
+++ src/test/java/be/ictdynamic/training/Spring2/Spring2ApplicationTests.java
@@ -0,0 +1,16 @@
+package be.ictdynamic.training.Spring2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
